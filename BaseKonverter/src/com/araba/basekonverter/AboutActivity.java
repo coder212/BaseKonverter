13a35f89e76af063a5a930ebd0468034d342ca32
arbaun zuhdi maarif <yujimaarif.ym@gmail.com>
2015-07-29 11:38:09 +0700

    create exception while invalidate number

---
+++ BaseKonverter/src/com/araba/basekonverter/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.araba.basekonverter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity  {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODOs Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.about);
+		
+	}
+	
+	
+	
+
+}
---
+++ BaseKonverter/src/com/araba/basekonverter/MainActivity.java
@@ -0,0 +1,183 @@
+package com.araba.basekonverter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+public class MainActivity extends Activity {
+
+	
+	EditText biner,decimal,octal,hexa;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODOs Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+		
+		biner=(EditText)findViewById(R.id.biner);
+		octal=(EditText)findViewById(R.id.octal);
+		decimal=(EditText)findViewById(R.id.decimal);
+		hexa=(EditText)findViewById(R.id.hexa);
+		
+		biner.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent event) {
+				// TODOs Auto-generated method stub
+				if (arg1==EditorInfo.IME_ACTION_DONE) {
+			           // the user is done typing.
+					String bil_Biner = biner.getText().toString();
+					try{
+						int intbiner = Integer.parseInt(bil_Biner,2);
+						MainActivity.this.octal.setText(Integer.toOctalString(intbiner));
+						MainActivity.this.hexa.setText(Integer.toHexString(intbiner));
+						MainActivity.this.decimal.setText(String.format("%d", intbiner));
+					}catch (Exception e){
+						e.printStackTrace();
+						binerError();
+					}
+					
+
+			           return true; // consume.
+			        }  
+				return false;
+			}
+		});
+		
+		octal.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
+				// TODOs Auto-generated method stub
+				if(arg1==EditorInfo.IME_ACTION_DONE){
+					
+					String bil_Octal = octal.getText().toString();
+					try{
+						int intoctal = Integer.parseInt(bil_Octal,8);
+						MainActivity.this.biner.setText(Integer.toBinaryString(intoctal));
+						MainActivity.this.hexa.setText(Integer.toHexString(intoctal));
+						MainActivity.this.decimal.setText(Integer.toString(intoctal));
+					}catch(Exception e){
+						e.printStackTrace();
+						octalError();
+					}
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		decimal.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
+				// TODOs Auto-generated method stub
+				if(arg1==EditorInfo.IME_ACTION_DONE){
+					
+				    try{
+					   int bil_Des = Integer.parseInt(decimal.getText().toString());
+					   //int intbiner = Integer.parseInt(bil_Biner);
+					   MainActivity.this.octal.setText(Integer.toOctalString(bil_Des));
+					   MainActivity.this.hexa.setText(Integer.toHexString(bil_Des));
+					   MainActivity.this.biner.setText(Integer.toBinaryString(bil_Des));
+				    }catch(Exception e){
+				    	e.printStackTrace();
+				    	decimalError();
+				    }
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		hexa.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
+				// TODOs Auto-generated method stub
+				if(arg1==EditorInfo.IME_ACTION_DONE){
+					
+					String bil_Hexa = hexa.getText().toString();
+					
+					try{
+					    int inthex = Integer.parseInt(bil_Hexa,16);
+						MainActivity.this.octal.setText(Integer.toOctalString(inthex));
+						MainActivity.this.biner.setText(Integer.toBinaryString(inthex));
+						MainActivity.this.decimal.setText(Integer.toString(inthex));
+					}catch(Exception e){
+						e.printStackTrace();
+						hexaError();
+					}
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		
+	}
+	
+	private void binerError(){
+		MainActivity.this.octal.setText("invalid number format");
+		MainActivity.this.hexa.setText("invalid number format");
+		MainActivity.this.decimal.setText("invalid number format");
+	}
+	
+	private void octalError(){
+		MainActivity.this.biner.setText("invalid number format");
+		MainActivity.this.hexa.setText("invalid number format");
+		MainActivity.this.decimal.setText("invalid number format");
+	}
+	
+	private void decimalError(){
+		MainActivity.this.octal.setText("invalid number format");
+		MainActivity.this.hexa.setText("invalid number format");
+		MainActivity.this.biner.setText("invalid number format");
+	}
+
+	private void hexaError(){
+	
+		MainActivity.this.octal.setText("invalid number format");
+		MainActivity.this.biner.setText("invalid number format");
+		MainActivity.this.decimal.setText("invalid number format");
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODOs Auto-generated method stub
+		
+		MenuInflater meninflater = getMenuInflater();
+		meninflater.inflate(R.menu.menu, menu);
+		
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODOs Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.about:
+			startActivity(new Intent(MainActivity.this,AboutActivity.class));
+			return true;
+		
+		case R.id.credits:
+			startActivity(new Intent(MainActivity.this,CreditsActivity.class));
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		
+	}
+
+}
